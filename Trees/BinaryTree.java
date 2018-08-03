6fa3c3185af1fb3847419d7e68f9c5cd5874733c
Saurabh Madhu Singh <singhsaurav727@gmail.com>
2018-08-03 18:50:26 +0530

    Update BinaryTree.java

--- Trees/BinaryTree.java
+++ Trees/BinaryTree.java
@@ -8,42 +8,83 @@ class Node{
         this.right=null;
     }
 }
-public class BinaryTree {
+public class BinarySearchTree {
     Node root;
-    BinaryTree(){
+    BinarySearchTree(){
         root=null;
     }
-    //method to insert into tree;
-    Node insertNode(Node root,int value){
+    //method to insert into the tree
+    void insertNode(Node rootNode, int data){
         if(root==null){
-            root= new Node(value);
+            Node newNode=new Node(data);
+            root=newNode;
         }
-        else if(value<root.data){
-            root.left=insertNode(root.left,value);
+        //move to left child
+        else if(data<rootNode.data){
+            if(rootNode.left==null) {
+                Node newNode= new Node(data);
+                rootNode.left=newNode;
+            }
+            else {
+                insertNode(rootNode.left,data);
+            }
         }
-        else if(value>root.data){
-            root.right=insertNode(root.right,value);
+        //move to right child
+        else if(data>rootNode.data){
+            if(rootNode.right==null){
+                Node newNode=new Node(data);
+                rootNode.right=newNode;
+            }
+            else{
+                insertNode(rootNode.right,data);
+            }
         }
-        return root;
     }
 
-    //inorder traversal
-    void inorder(Node root){
-        if(root!=null){
-            inorder(root.left);
-            System.out.println(root.data+"->");
-            inorder(root.right);
+    //method for inorder traversal
+    void inorder(Node currentNode){
+        if(currentNode!=null){
+            inorder(currentNode.left);
+            System.out.print(currentNode.data+"->");
+            inorder(currentNode.right);
+        }
+    }
+
+    //method for preorder traversal
+    void preorder(Node currentNode){
+        if(currentNode!=null){
+            System.out.print(currentNode.data+"->");
+            preorder(currentNode.left);
+            preorder(currentNode.right);
+        }
+    }
+
+    //method for postorder traversal
+    void postorder(Node currentNode){
+        if(currentNode!=null){
+            postorder(currentNode.right);
+            System.out.print(currentNode.data+"->");
+            postorder(currentNode.right);
         }
     }
 }
 
-class main{
+class Main{
     public static void main(String[] args) {
         int array[]={45,39,56,12,34,78,32,10,89,54,67,81};
-        BinaryTree bt= new BinaryTree();
-        for(int i=0;i<10;i++){
-            bt.insertNode(bt.root,array[i]);
+        BinarySearchTree bst= new BinarySearchTree();
+        for(int i=0;i<array.length;i++){
+            bst.insertNode(bst.root,array[i]);
         }
-        bt.inorder(bt.root);
+        //inorder traversal
+        System.out.println("Inorder Traversal");
+        bst.inorder(bst.root);
+        //preorder traversal
+        System.out.println("\nPreorder traversal");
+        bst.preorder(bst.root);
+        //postorder traversal
+        System.out.println("\nPostorder Traversal");
+        bst.postorder(bst.root);
+
     }
 }
