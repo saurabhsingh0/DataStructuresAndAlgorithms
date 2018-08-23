5cde51629d9e01d978b758e7874cef15a75179e1
Saurabh Singh <singhsaurav727@gmail.com>
2018-08-23 23:50:53 +0530

    Divide and Conquer

--- Socket Programming/client.java
+++ Socket Programming/client.java
@@ -1,1 +1,10 @@
-client.java
+import java.io.*;
+import java.net.*;
+public class client{
+	Soket S= new Socket("localhost",6666);
+	DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+	dout.writeUTF("Hello Server");
+	dout.flush();
+	dout.close();
+	s.close();
+}
